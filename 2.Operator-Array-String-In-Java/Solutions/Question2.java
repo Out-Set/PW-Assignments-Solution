ce6d9fb3fa58a9dde3efa6ef5b1fb328bed89ed4
swk802@gmail.com <swk802@gmail.com>
2023-12-26 00:20:11 +0530

    2nd assignment done

---
+++ 2.Operator-Array-String-In-Java/Solutions/Question2.java
@@ -0,0 +1,29 @@
+class Question2 {
+	public static void main(String[] args) {
+		
+		// Pre Increment
+		int b = 5;
+		System.out.println(++b); // 6
+		System.out.println(b);   // 6
+		System.out.println();
+
+		// Post Increment
+		int a = 5;
+		System.out.println(a++); // 5
+		System.out.println(a);   // 6
+		System.out.println();
+
+		
+		// Pre Decrement
+		int c = 5;
+		System.out.println(--c); // 4
+		System.out.println(c);   // 4
+		System.out.println();
+
+		// Post Decrement
+		int d = 5;
+		System.out.println(d--); // 5
+		System.out.println(d);   // 4	
+		System.out.println();
+	}
+}
